ed9529ec9d8131afde0c43741f609e83ec8f9f1a
glafirov <glafirov@sibers.com>
2017-05-31 09:53:35 +0600

    Developed mapping

---
+++ TasklistApp.Web/TasklistApp.Web/App_Start/AutomapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TasklistApp.Web.Mapping;
+
+namespace TasklistApp.Web.App_Start {
+    public class AutomapperConfig {
+        public static void CreateMap() {
+            Mapper.Initialize(cfg => cfg.AddProfile<WebMappingProfile>());
+            Mapper.AssertConfigurationIsValid();
+        }
+    }
+}
---
+++ TasklistApp.Web/TasklistApp.Web/Mapping/WebMappingProfile.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TaskListApp.Common.Extensions;
+using TaskListApp.Contracts.DtoModels;
+using TaskListApp.Domain.Models;
+using TaskListApp.Domain.Models.Identity;
+using TaskListApp.Web.Areas.Account.Models.AccountViewModels;
+using TaskListApp.Web.Areas.Admin.Models.DepartmentsViewModels;
+using TaskListApp.Web.Areas.Admin.Models.UsersViewModels;
+using TaskListApp.Web.Areas.User.Models.ToDoTasksViewModels;
+
+namespace TasklistApp.Web.Mapping {
+    public class WebMappingProfile : Profile {
+
+        public WebMappingProfile() {
+
+            #region [ Admin area ]
+
+            #region [ Department models ]
+
+            CreateMap<DepartmentViewModel, Department>().IgnoreAllNonExisting().ReverseMap().IgnoreAllNonExisting();
+            CreateMap<DepartmentBlankViewModel, Department>().IgnoreAllNonExisting().ReverseMap().IgnoreAllNonExisting();
+            CreateMap<Department, DepartmentViewModel>().IgnoreAllNonExisting().ReverseMap().IgnoreAllNonExisting();
+            CreateMap<Department, DepartmentBlankViewModel>().IgnoreAllNonExisting().ReverseMap().IgnoreAllNonExisting();
+
+            #endregion // [ Department models ]
+
+            #region [ Users models ]
+
+            CreateMap<UserViewModel, User>().IgnoreAllNonExisting().ReverseMap().IgnoreAllNonExisting();
+            CreateMap<User, UserViewModel>().IgnoreAllNonExisting().ReverseMap().IgnoreAllNonExisting();
+
+            #endregion //[ Users models ]
+
+            #endregion //[ Admin area ]
+
+            #region [ User area ]
+
+            #region [ ToDoTasks models ]
+
+            CreateMap<ToDoTaskViewModel, ToDoTask>().IgnoreAllNonExisting().ReverseMap().IgnoreAllNonExisting();
+            CreateMap<ToDoTaskIndexViewModel, ToDoTask>().IgnoreAllNonExisting().ReverseMap().IgnoreAllNonExisting();
+            CreateMap<ToDoTaskDetailViewModel, ToDoTask>().IgnoreAllNonExisting().ReverseMap().IgnoreAllNonExisting();
+            CreateMap<ToDoTaskBlankViewModel, ToDoTask>().IgnoreAllNonExisting().ReverseMap().IgnoreAllNonExisting();
+            CreateMap<ToDoTask, ToDoTaskViewModel>().IgnoreAllNonExisting().ReverseMap().IgnoreAllNonExisting();
+            CreateMap<ToDoTask, ToDoTaskIndexViewModel>().IgnoreAllNonExisting().ReverseMap().IgnoreAllNonExisting();
+            CreateMap<ToDoTask, ToDoTaskDetailViewModel>().IgnoreAllNonExisting().ReverseMap().IgnoreAllNonExisting();
+            CreateMap<ToDoTask, ToDoTaskBlankViewModel>().IgnoreAllNonExisting().ReverseMap().IgnoreAllNonExisting();
+
+            #endregion //[ ToDoTasks models ]
+
+            #endregion //[ User area ]
+
+            #region [ Account models ]
+
+            CreateMap<RegisterViewModel, UserDto>().IgnoreAllNonExisting().ReverseMap().IgnoreAllNonExisting();
+            CreateMap<LoginViewModel, UserDto>().IgnoreAllNonExisting().ReverseMap().IgnoreAllNonExisting();
+
+            #endregion //[ Account models ]
+
+
+        }
+
+    }
+}
