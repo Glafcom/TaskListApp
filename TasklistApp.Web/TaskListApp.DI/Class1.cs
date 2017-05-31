65e1df02e3187c6d477d10a068a63fcc318482f7
glafirov <glafirov@sibers.com>
2017-05-31 21:32:14 +0700

    Developed DI

---
+++ TasklistApp.Web/TaskListApp.DI/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskListApp.DI
+{
+    public class Class1
+    {
+    }
+}
---
+++ TasklistApp.Web/TaskListApp.Infrastructure/ContainerExtensions/TaskListAppContainerExtension.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskListApp.BLL.Domains;
+using TaskListApp.BLL.Identity;
+using TaskListApp.BLL.Services;
+using TaskListApp.Contracts.BLLContracts.Domains;
+using TaskListApp.Contracts.BLLContracts.Services;
+using TaskListApp.Contracts.DALContracts;
+using TaskListApp.Contracts.DALContracts.Identity;
+using TaskListApp.DAL.AppDbContext;
+using TaskListApp.DAL.Repositories;
+using TaskListApp.Domain.Models;
+using TaskListApp.Domain.Models.Identity;
+
+namespace TaskListApp.Infrastructure.ContainerExtensions {
+    public class TaskListAppContainerExtension : UnityContainerExtension {
+
+        protected override void Initialize() {
+
+            #region [ BLL Layer ]
+
+            #region [ Services ]
+
+            Container.RegisterType<IAccountService, AccountService>(new PerRequestLifetimeManager());
+            Container.RegisterType<IDepartmentService, DepartmentService>(new PerRequestLifetimeManager());
+            Container.RegisterType<IToDoTaskService, ToDoTaskService>(new PerRequestLifetimeManager());
+            Container.RegisterType<IUserService, UserService>(new PerRequestLifetimeManager());
+
+            #endregion //[ Services ]
+
+            #region [ Domains ]
+
+            Container.RegisterType<IAdminDomain, AdminDomain>(new PerRequestLifetimeManager());
+            Container.RegisterType<IUserDomain, UserDomain>(new PerRequestLifetimeManager());
+
+            #endregion //[ Domains ]
+
+            #endregion //[ BLL Layer ] 
+
+            #region [ DAL Layer ]
+
+
+            Container.RegisterType<TaskListAppDbContext>(new PerRequestLifetimeManager(), new InjectionConstructor("TaskListAppDbConnection"));
+            Container.RegisterType<DbContext, TaskListAppDbContext>(new PerRequestLifetimeManager());
+            Container.RegisterType<IApplicationSignInManager, ApplicationSignInManager>(new PerRequestLifetimeManager());
+            Container.RegisterType<IApplicationUserManager, ApplicationUserManager>(new PerRequestLifetimeManager());
+
+            #region [ Repositories ]
+
+            Container.RegisterType<IGenericRepository<User>, GenericRepository<User>>(new PerRequestLifetimeManager());
+            Container.RegisterType<IGenericRepository<Department>, GenericRepository<Department>>(new PerRequestLifetimeManager());
+            Container.RegisterType<IGenericRepository<ToDoTask>, GenericRepository<ToDoTask>>(new PerRequestLifetimeManager());
+
+            #endregion // [ Repositories ]
+
+            #endregion // [ DAL Layer ]
+        }
+    }
+}
