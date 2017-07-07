35b9264a22b94c4f50d039291ad99d557877473a
glafirov <dmitriy.glafirov@sibers.com>
2017-07-07 13:36:17 +0700

    Corrected something

--- TasklistApp.Web/TasklistApp.Web/Behaviors/AuthBehavior.cs
+++ TasklistApp.Web/TasklistApp.Web/Behaviors/AuthBehavior.cs
@@ -6,6 +6,27 @@ using System.Web;
 
 namespace TasklistApp.Web.Behaviors {
     public class AuthBehavior : IInterceptionBehavior {
+        public IEnumerable<Type> GetRequiredInterfaces() {
+            return Type.EmptyTypes;
+        }
 
+        public bool WillExecute {
+            get { return true; }
+        }
+
+        public IMethodReturn Invoke(IMethodInvocation input, GetNextInterceptionBehaviorDelegate getNext) {
+
+            if (input.MethodBase .Name == "GetTenant") {
+                var tenantName = input.Arguments["tenant"].ToString();
+                if (IsInCache(tenantName)) {
+                    return input.CreateMethodReturn(
+                      FetchFromCache(tenantName));
+                }
+            }
+
+            var result = getNext().Invoke(input, getNext);
+            
+            return result;
+        }
     }
 }
